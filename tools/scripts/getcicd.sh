98b0ba22bc39e3124c9177d5277862490df63573
Matthew Campbell <mjcampbell@vmware.com>
2021-12-15 16:49:07 -0500

    update for minikube-a01 environment

--- tools/scripts/getcicd.sh
+++ tools/scripts/getcicd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Update these to match your environment
 export SERVICE_ACCOUNT_NAME=kpack-deploy-sa
-export NAMESPACE=default
+export NAMESPACE=jenkins
 export NEW_CONTEXT=kpack-deploy-sa
 
 ./getkube.sh $SERVICE_ACCOUNT_NAME $NAMESPACE $NEW_CONTEXT
